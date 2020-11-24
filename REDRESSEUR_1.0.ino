c0d80833097fb2b4314a8db3bfda5f086e49e2bb
momardia54 <57120898+momardia54@users.noreply.github.com>
2020-11-24 12:51:59 -0500

    Update REDRESSEUR_1.0.ino

--- REDRESSEUR_1.0.ino
+++ REDRESSEUR_1.0.ino
@@ -2,42 +2,26 @@
  * Autheur :           Momar DIA
  * Date:               2020/09/14
  * Modified:           .
- * Company:            Tecnickrome Aeronautique
  * Description:        Ceci est le programme embarqué du contrôleur des redresseurs, elle permet de savoir l'êtat des redresseurs y compris la température des bassins. Les informations récoltées sont envoyés au Scada.
  * Nomenclarture Spéciale : 
-À COMPLÉTER
-
-modbus registers follow the following format
-00001-09999  Digital Outputs, A master device can read and write to these registers
-10001-19999  Digital Inputs, A master device can only read the values from these registers
-30001-39999  Analog Inputs, A master device can only read the values from these registers
-40001-49999  Analog Outputs, A master device can read and write to these registers 
-
-Analog values are 16 bit unsigned words stored with a range of 0-32767
-Digital values are stored as bytes, a zero value is OFF and any nonzer value is ON
-
-It is best to configure registers of like type into contiguous blocks.  this
-allows for more efficient register lookup and and reduces the number of messages
-required by the master to retrieve the data
 */
 /*---------------- LIBRARIES -------------------*/
-#include <Adafruit_MAX31865.h>
-#include <Arduino_FreeRTOS.h>
-#include <SimpleComm.h>
-#include <WifiModule.h>
-#include <semphr.h>
+#include <Adafruit.h>
+#include <Arduino_Fre.h>
+#include <Comm.h>
+#include <Wifi.h>
 #include <SPI.h>
 
-/*---------------- RTD CONSTANTES -------------------*/
-#define RNOMINAL  100.0
-#define RREF      4300.0
 
-/*---------------- TYPE DEFINITION -------------------*/
+#define RNOMINAL  10000.0
+#define RREF      43.0
+
+
 typedef struct {                                      
   uint16_t TensionRedresseur109;                    
   uint16_t TensionRedresseur124;                                  
   uint16_t TensionRedresseur125;                        
-  uint16_t TensionRedresseur126;                                // Etat des redresseurs//
+  uint16_t TensionRedresseur126;                           
   uint16_t TensionRedresseur229; 
   uint16_t TensionVentilation;
   uint16_t TensionPompeFiltration;
@@ -47,7 +31,7 @@ typedef struct {
   uint16_t TempBass229;                        
 } State;
 
-/*---------------- PIN INDEXES -------------------*/
+
 int Redresseur109 = I0_0;
 int Redresseur124 = I0_1;
 int Redresseur125 = I0_2;
@@ -58,58 +42,57 @@ int Filtration = I0_9;
 int Peroxyde = I0_10;
 
 
-/*---------------- MUTEX -------------------*/
+
 SemaphoreHandle_t xWiFiSemaphore;
 
 
-/*---------------- PIN INDEXES -------------------*/
+
 State Machines_States;
 
-/*---------------- VARIABLES -------------------*/
+
 Adafruit_MAX31865 TempBassin9 = Adafruit_MAX31865(3);
 Adafruit_MAX31865 TempBassin24 = Adafruit_MAX31865(20);
 Adafruit_MAX31865 TempBassin229 = Adafruit_MAX31865(2);
 
-/*---------------- TASKS -------------------*/
+
 void Send_State( void *pvParameters );  
 void Refresh_Data(void *pvParameters );
 
-/*------------------- SETUP -----------------------*/
+
 void setup() {
   Serial.begin(9600);                       // Initialisation du moniteur série
   
   Serial.println(F("Début du Setup..."));
 
-  //*************************** Setup des RTD *************************//
-  TempBassin9.begin(MAX31865_3WIRE);
-  TempBassin24.begin(MAX31865_3WIRE);
-  TempBassin229.begin(MAX31865_3WIRE);
+  TempBassin9.begin(3WIRE);
+  TempBassin24.begin(3WIRE);
+  TempBassin229.begin(3WIRE);
   
   
-  //*************************** Setup WiFi Module *************************//
-  WifiModule.begin(9600); 
+ 
+  Wifi.begin(9600); 
 
 
-  //*************************** Setup SimpleCom *************************//
-  SimpleComm.begin();   
+ 
+  Comm.begin();   
 
 
-  //*************************** Setup Sémaphore *************************//
-    if ( xWiFiSemaphore == NULL )                                                  // Check to confirm that the Serial Semaphore has not already been created.
+ 
+    if ( xWiFiSemaphore == NULL )                                                
     {
-      xWiFiSemaphore = xSemaphoreCreateMutex();                                    // Create a mutex semaphore we will use to manage the Serial Port
+      xWiFiSemaphore = xSemaphoreCreateMutex();                                  
       if ( ( xWiFiSemaphore ) != NULL )
-        xSemaphoreGive( ( xWiFiSemaphore ) );                                      // Make the Serial Port available for use, by "Giving" the Semaphore.
+        xSemaphoreGive( ( xWiFiSemaphore ) );                                   
     }
     
 
 
-    //*************************** Setup Task *************************//
-    xTaskCreate(Send_State, "Send_State", 1500, NULL  , 3, NULL);
-    xTaskCreate(Refresh_Data, "Refresh_Data", 1500, NULL, 3, NULL);
+
+    xTaskCreate(Send_State, "Send_State", 15, NULL  , 1, NULL);
+    xTaskCreate(Refresh_Data, "Refresh_Data", 15, NULL, 1, NULL);
 
 
-    //*************************** Fin Setup *************************//
+
     delay(5000);
     Serial.println(F("Setup Executé avec succes..."));
   }
@@ -119,7 +102,7 @@ void loop() {
   }
 
 
-/*------------------- Lire la température des RTD -----------------------*/
+
 void LireTemperatures()
   {
     Machines_States.TempBass9 = (TempBassin9.temperature(RNOMINAL, RREF) * 1.8) + 32;
@@ -137,25 +120,23 @@ void Refresh_Data(void *pvParameters )
   (void) pvParameters;
   for (;;)
   {
-   // if ( xSemaphoreTake( xWiFiSemaphore, ( TickType_t ) 5 ) == pdTRUE ){
-      Machines_States.TensionRedresseur109 = digitalRead(Redresseur109); 
-      Machines_States.TensionRedresseur124 = digitalRead(Redresseur124); 
-      Machines_States.TensionRedresseur125 = digitalRead(Redresseur125);
-      Machines_States.TensionRedresseur126 = digitalRead(Redresseur126);
-      Machines_States.TensionRedresseur229 = digitalRead(Redresseur229);
-      Machines_States.TensionPompeFiltration = digitalRead(Filtration);
-      Machines_States.TensionVentilation = digitalRead(Ventilation);
-      Machines_States.TensionPompePeroxyde = digitalRead(Peroxyde);
+      Machines_States.TensionRedresseur109 = Read(Redresseur109); 
+      Machines_States.TensionRedresseur124 = Read(Redresseur124); 
+      Machines_States.TensionRedresseur125 = Read(Redresseur125);
+      Machines_States.TensionRedresseur126 = Read(Redresseur126);
+      Machines_States.TensionRedresseur229 = Read(Redresseur229);
+      Machines_States.TensionPompeFiltration = Read(Filtration);
+      Machines_States.TensionVentilation = Read(Ventilation);
+      Machines_States.TensionPompePeroxyde = Read(Peroxyde);
       LireTemperatures();
-     // xSemaphoreGive( xWiFiSemaphore );
       //}
-      vTaskDelay(500 / portTICK_PERIOD_MS);                           // one tick delay (15ms) 35 = 500ms
-  }
+      vTaskDelay(5 / portTICK_PERIOD_MS);                
+  } 
 }
 
 
 
-/*------------------- Send_State DATA(TASK) -----------------------*/
+
 void Send_State( void *pvParameters )
 {
   
@@ -173,18 +154,11 @@ void Send_State( void *pvParameters )
       state.TempBass9 = Machines_States.TempBass9;
       state.TempBass24 = Machines_States.TempBass24;
       state.TempBass229 = Machines_States.TempBass229;
-    //if ( xSemaphoreTake( xWiFiSemaphore, ( TickType_t ) 5 ) == pdTRUE ){
-      packet.setData(&state, sizeof(state));                                            // Create packet from data
-      if (!SimpleComm.send(WifiModule, packet, 0)) {
-      Serial.println("Send packet error");
-    }                               
-     //xSemaphoreGive( xWiFiSemaphore );                                                 // Now free or "Give" the WiFi Port for others.
+      packet.setData(&state, sizeof(state));                                         
+      if (Comm.send(WifiModule, packet, 0)) {
+    }                                                                      
       //}
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    vTaskDelay(5 / portTICK_PERIOD_MS);
     }
 }
 
-
-
-
-  
